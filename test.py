cf12e87284222432e34cad9c69feca767b9061ec
drybonez01 <83592470+drybonez01@users.noreply.github.com>
2023-12-19 15:01:42 +0100

    enhanced train.py and test.py also switched from CoNaLa to CoNaLa-Large and to the google/flan-t5-base model

--- test.py
+++ test.py
@@ -2,24 +2,15 @@ from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 
 
 def test():
-    new_model = AutoModelForSeq2SeqLM.from_pretrained("./Marian-Training")
-    new_model.to("cuda")
+    new_model = AutoModelForSeq2SeqLM.from_pretrained("Fine-Tuned-Models/checkpoint-33375")
+    new_tokenizer = AutoTokenizer.from_pretrained("Fine-Tuned-Models/checkpoint-33375", use_fast=True)
 
-    new_tokenizer = AutoTokenizer.from_pretrained("./Marian-Training", use_fast=True)
+    prompt = input("Prompt: ")
 
-    def generate_code(prompt):
-        inputs = new_tokenizer([prompt], padding="max_length", truncation=True, max_length=512, return_tensors="pt")
+    inputs = new_tokenizer([prompt], padding="max_length", truncation=True, max_length=512, return_tensors="pt")
+    input_ids = inputs.input_ids
+    attention_mask = inputs.attention_mask
+    outputs = new_model.generate(input_ids, attention_mask=attention_mask)
+    generated_code = new_tokenizer.batch_decode(outputs, skip_special_tokens=True)
 
-        input_ids = inputs.input_ids.to("cuda")
-        attention_mask = inputs.attention_mask.to("cuda")
-
-        outputs = new_model.generate(input_ids, attention_mask=attention_mask)
-
-        output_code = new_tokenizer.batch_decode(outputs, skip_special_tokens=True)
-
-        return output_code
-
-    natural_language = input()
-    gen_code = generate_code(natural_language)
-
-    print(gen_code)
+    print(generated_code)
--- train.py
+++ train.py
@@ -1,91 +1,51 @@
-import transformers
-import datasets
-from datasets import load_dataset, load_metric
-import logging
-from transformers import BertTokenizer, GPT2Tokenizer, GPT2TokenizerFast, EncoderDecoderModel, Trainer, TrainingArguments, BertTokenizerFast
-from transformers import Seq2SeqTrainer, Seq2SeqTrainingArguments
 from transformers import AutoModelForSeq2SeqLM, DataCollatorForSeq2Seq, Seq2SeqTrainingArguments, Seq2SeqTrainer
-import types
-import argparse
 import logging
-from functools import partial
-import json
-
 import torch
-from torch import optim
-from torch.utils.data import DataLoader
-from tqdm import tqdm
-from transformers import (
-    BertGenerationConfig,
-    BertGenerationEncoder,
-    BertTokenizer,
-    EncoderDecoderModel,
-    EncoderDecoderConfig,
-    GPT2LMHeadModel,
-    GPT2TokenizerFast,
-    Trainer,
-    TrainingArguments,
-    get_cosine_schedule_with_warmup,
-    AutoTokenizer
-)
-
-import sacrebleu
-import random
-import numpy as np
+from transformers import AutoTokenizer
 import pandas as pd
 from datasets import Dataset
-from utils import evaluator as E
+from utils import evaluator as e
+
+tokenizer = AutoTokenizer.from_pretrained(
+        # "Helsinki-NLP/opus-mt-en-nl",
+        "google/flan-t5-base",
+        use_fast=True)
+
+
+def preprocess(examples, max_length=512):
+    model_inputs = tokenizer(examples['intent'],
+                             max_length=max_length,
+                             padding='max_length',
+                             truncation=True,
+                             return_attention_mask=True)
+    with tokenizer.as_target_tokenizer():
+        targets = tokenizer(examples['snippet'],
+                            max_length=max_length,
+                            padding='max_length',
+                            truncation=True,
+                            return_attention_mask=True)
+    model_inputs['labels'] = targets['input_ids']
+    model_inputs['decoder_input_ids'] = targets['input_ids']
+    model_inputs['decoder_attention_mask'] = targets['attention_mask']
+    return model_inputs
 
 
 def train():
-    def preprocess(examples, MAX_LENGTH=512):
-        model_inputs = tokenizer(examples['intent'],
-                                 max_length=MAX_LENGTH,
-                                 padding='max_length',
-                                 truncation=True,
-                                 return_attention_mask=True)
-        with tokenizer.as_target_tokenizer():
-            targets = tokenizer(examples['snippet'],
-                                max_length=MAX_LENGTH,
-                                padding='max_length',
-                                truncation=True,
-                                return_attention_mask=True)
-        model_inputs['labels'] = targets['input_ids']
-        model_inputs['decoder_input_ids'] = targets['input_ids']
-        model_inputs['decoder_attention_mask'] = targets['attention_mask']
-        return model_inputs
-
     logging.basicConfig(level=logging.INFO)
 
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
-    raw_dataset = datasets.load_dataset(
-        'csv',
-        data_files={
-            'train': './CoNaLa/train.csv',
-            'validation': './CoNaLa/val.csv',
-            'test': './CoNaLa/test.csv'},
-        # delimeter=',',
-        quotechar='"')
-
-    train_df = pd.read_csv('CoNaLa/train.csv')
-    val_df = pd.read_csv('CoNaLa/val.csv')
-    test_df = pd.read_csv('CoNaLa/test.csv')
+    train_df = pd.read_csv('CoNaLa-Large/train.csv')
+    val_df = pd.read_csv('CoNaLa-Large/val.csv')
 
     train_dataset = Dataset.from_pandas(train_df)
     val_dataset = Dataset.from_pandas(val_df)
-    test_dataset = Dataset.from_pandas(test_df)
 
-    tokenizer = AutoTokenizer.from_pretrained(
-        "Helsinki-NLP/opus-mt-en-nl",
-        use_fast=True)
+    # model = AutoModelForSeq2SeqLM.from_pretrained("Helsinki-NLP/opus-mt-en-nl")
+    model = AutoModelForSeq2SeqLM.from_pretrained("google/flan-t5-base")
+    evaluator = e.CodeGenerationEvaluator(tokenizer, device, smooth_bleu=True)
 
-    model = AutoModelForSeq2SeqLM.from_pretrained("Helsinki-NLP/opus-mt-en-nl")
-    evaluator = E.CodeGenerationEvaluator(tokenizer, device, smooth_bleu=True)
-
-    max_length = 512
     batch_size = 1
-    learning_rate = 1e-5
 
     train_inputs = train_dataset.map(preprocess, batched=True)
     val_inputs = val_dataset.map(preprocess, batched=True)
@@ -96,23 +56,14 @@ def train():
                                            max_length=512,
                                            model=model)
 
-    train_dataloader = torch.utils.data.DataLoader(train_inputs,
-                                                   batch_size=batch_size,
-                                                   shuffle=True,
-                                                   collate_fn=data_collator)
-    val_dataloader = torch.utils.data.DataLoader(val_inputs,
-                                                 batch_size=batch_size,
-                                                 shuffle=False,
-                                                 collate_fn=data_collator)
-
     training_args = Seq2SeqTrainingArguments(
-        output_dir="./Marian-Training",
+        output_dir="Fine-Tuned-Models",
         evaluation_strategy="epoch",
         save_strategy="epoch",
         per_device_train_batch_size=batch_size,
         per_device_eval_batch_size=batch_size,
         predict_with_generate=True,
-        num_train_epochs=3,
+        num_train_epochs=5,
         do_train=True,
         do_eval=True,
         fp16=True,
@@ -137,4 +88,3 @@ def train():
 
     trainer.train()
     trainer.save_model()
-    # trainer.save_state()
